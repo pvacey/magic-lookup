fa15f807f7adf310bf9b5bbf72ee504664ba6f99
Paul Vacey <pvacey@gmail.com>
2024-01-07 15:46:03 -0600

    no mouseover actions

--- src/Card.svelte
+++ src/Card.svelte
@@ -4,18 +4,6 @@
     export let cardSpace;
     const dispatch = createEventDispatcher();
 
-    function mouseEnterCard() {
-        dispatch('message', {
-            type: 'mouseenter',
-            data: data,
-        });
-    }
-    function mouseLeaveCard() {
-        dispatch('message', {
-            type: 'mouseleave',
-            data: data
-        });
-    }
     function clickCard() {
         dispatch('message', {
             type: 'click',
@@ -27,6 +15,6 @@
 
 <div class="cardspacer" style="height: {cardSpace}px; margin-top: -{cardSpace/4}px">
     <div class="card" style="transform: rotate({(Math.random()*1.5) -1}deg) translateX({Math.random()*10}px);" >
-        <img src={data[data.active]} alt="" on:mouseenter={mouseEnterCard} on:mouseleave={mouseLeaveCard} on:click={clickCard}>
+        <img src={data[data.active]} on:click={clickCard}/>
     </div>
 </div>
